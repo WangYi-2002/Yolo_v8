63ebf7e0948ddbc928eec4eeafa2d9687ae28cf5
DmWang02 <1813360323@qq.com>
2025-03-23 20:10:58 +0800

    Auto commit 2025-03-23 20:10:58

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")  # 确保文件路径正确
-model.train(data="../fruits/data.yaml", epochs=3)
+model.train(data="D:/Yolo_model/Yolo_v8/fruits/data.yaml", epochs=3)
 model.val()
 model.export(format="onnx")
